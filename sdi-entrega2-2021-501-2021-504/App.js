6879fd20557544fd2229f9f26294a397807eb63a
Eric Almeda Tomás <UO269947@uniovi.es>
2021-05-04 10:48:31 +0200

    Hecho login y registro

--- sdi-entrega2-2021-501-2021-504/App.js
+++ sdi-entrega2-2021-501-2021-504/App.js
@@ -1,14 +1,47 @@
+//Modulos
 let express = require('express');
 let app = express();
+
+//Módulo express-session
+let expressSession = require('express-session');
+app.use(expressSession({
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: true
+}));
+
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
 let swig = require('swig');
 let crypto = require('crypto');
 let mongo = require('mongodb');
 
-require("./routes/rofertas.js")(app, swig);
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
 
+
+require("./routes/rofertas.js")(app, swig,gestorBD);
+require("./routes/rusuarios.js")(app, swig,gestorBD);
+
+
+app.use(function (err,req,res,next) {
+    console.log("Error producido: "+err);//mostramos el error en consola
+    if(!res.headersSent){
+        res.status(400);
+        res.send("Recurso no disponible");
+    }
+
+});
+
+//Variables
 app.set('port', 8081);
+app.set('db','mongodb://admin:admin@cluster0-shard-00-00.urjej.mongodb.net:27017,cluster0-shard-00-01.urjej.mongodb.net:27017,cluster0-shard-00-02.urjej.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-ojwuzg-shard-0&authSource=admin&retryWrites=true&w=majority');
+app.set('crypto',crypto);
+app.set('clave','abcdefg');
 
 app.listen(app.get('port'), function() {
     console.log("Servidor activo");
